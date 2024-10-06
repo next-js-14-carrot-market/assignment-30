430881b0e7b25326406555e5acde734e166b6c86
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-07 05:11:06 +0900

    feat: Assignment#30 - Tweets
    - / 페이지를 구현
    - 다음 페이지로 이동하거나 이전 페이지로 돌아갈 수 있도록 화살표를 표시하는 페이지네이션(pagination)을 구현
    - 트윗을 클릭하면 /tweets/[id] 트윗의 상세 페이지 이동

--- src/app/create-account/actions.ts
+++ src/app/create-account/actions.ts
@@ -98,5 +98,5 @@ export async function handleForm(_: unknown, formData: FormData): Promise<FormSt
   const session = await getSession();
   session.id = user.id;
   await session.save();
-  redirect("/profile");
+  redirect("/");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-"use client";
+import TweetList from "@/components/tweet-list";
+import { getInitialTweets } from "../service/tweetService";
 
-export default function Home() {
-  return <main className="flex flex-col gap-10 items-center justify-center">HOME</main>;
+export default async function MainPage() {
+  const tweets = await getInitialTweets();
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      <TweetList initialTweets={tweets} />
+    </div>
+  );
 }
